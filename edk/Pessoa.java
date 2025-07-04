118d5ae925cbfdd6d916213f5ca559d813ff46f2
EdklauseDiniz <edklausezinho@gmail.com>
2025-07-04 10:30:01 -0300

    Add files via upload

---
+++ edk/Pessoa.java
@@ -0,0 +1,47 @@
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private String cpf;
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public Pessoa(String nome, String cpf, int idade) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        if (!nome.isBlank()) {
+            this.nome = nome;
+        }
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        if (!cpf.isBlank()) {
+            this.cpf = cpf;
+        }
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int cpf) {
+        if (idade >= 0) {
+            this.idade = idade;
+        }
+    }
+
+}
